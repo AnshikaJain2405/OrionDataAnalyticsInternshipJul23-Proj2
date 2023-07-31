21693cf6b751fcc90f64b1a662e9e1508dacbba0
Anshika Jain <anshikajain2405@gmail.com>
2023-07-31 17:51:16 +0530

    added feature_engg(), changed remove_null_values

--- src/utils.py
+++ src/utils.py
@@ -4,23 +4,17 @@ import numpy as np
 def change(df, column):
     '''Removed the words bedroom, bhk and rk from the size column as we saw from above that we need 
     to do that and then created chart for better understanding of the values now.'''
-    df[column] = df[column].astype(str).str.replace(' Bedroom', '').str.replace(' BHK', '').str.replace(' RK', '')
-    return df[column]
+    df[column] = df[column].astype(str).str.replace(' Bedroom', '').str.replace(' BHK', '').str.replace(' RK', '').astype(float)
+    return df#[column]
 
 def remove_null_values(df:pd.DataFrame):
     '''Removing the null values in the data using mean, median, 
     or a particular value, based on initial analysis.'''
     df['size'].fillna(df['size'].astype(float).median(), inplace=True)
-    df['size'] = df['size'].astype(float)
-
     df['society'].fillna('others', inplace=True)
-
     df['bath'].fillna(df['bath'].astype(float).median(), inplace=True)
-
     df['balcony'].fillna(df['balcony'].astype(float).median(), inplace=True)
-
     df['location'].fillna(df['location'].mode()[0], inplace=True)
-
     return df
 
 
@@ -70,13 +64,14 @@ def Total_sqft_convert(value):
 def remove_skew(df, column):
     '''Removing skewdness of the data'''
     df[column]=np.log1p(df[column])
+    return df
 
 
 def preprocess_data(df):
     '''cleans the data according to our needs'''
-    change(df, 'size')
+    df = change(df, 'size')
 
-    remove_null_values(df)
+    df = remove_null_values(df)
 
     df['total_sqft'] = df['total_sqft'].apply(Total_sqft_average)
 
@@ -84,7 +79,28 @@ def preprocess_data(df):
 
     df['total_sqft'] = pd.to_numeric(df['total_sqft'], errors='coerce')
 
-    remove_skew(df, 'price')
-    remove_skew(df, 'total_sqft')
+    df = remove_skew(df, 'price')
+    df = remove_skew(df, 'total_sqft')
 
+    return df
+
+
+def extract_month(s):
+    '''Extracts month from dates in the column'''
+    return s.str.extract(r'(\b[a-zA-Z]{3}\b)')
+
+def extract_text(text):
+    '''Extracts words as they are from the column'''
+    if isinstance(text, str):
+        text_parts = text.split('-')
+        first_part = text_parts[0].strip()
+        if any(c.isalpha() for c in first_part):
+            return first_part
+    return np.nan
+
+def feature_engg(df):
+    '''Creates two new columns for month and ready'''
+    df['month'] = extract_month(df['availability'])
+    df['ready'] = df['availability'].apply(extract_text)
+    df['extract'] = df['ready'].fillna(df['month'])
     return df
