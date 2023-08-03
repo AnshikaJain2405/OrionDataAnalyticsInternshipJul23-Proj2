4760b04fcdd8cef15bf879368e88e205c32cb3d7
Anshika Jain <anshikajain2405@gmail.com>
2023-08-03 10:21:49 +0530

    added scikit-learn

---
+++ app/Predict_Prices_💸.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import joblib
+
+st.title('Bengaluru House Price Prediction')
+
+# st.sidebar.success("Select a demo above.")
+
+area_type = st.radio("What\'s your preferred area type",('Plot  Area', 'Super built-up  Area', 'Built-up  Area', 'Carpet  Area'))
+
+size = st.text_input("What is your preferred Number of Bedrooms")
+
+total_sqft = st.text_input("Total preferred area(in square ft.)")
+
+bath = st.text_input("What is your preferred Number of Bathrooms")
+
+balcony = st.text_input("What is your preferred Number of Balconies")
+
+extract = st.selectbox("When are you looking to move in",('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec','Ready to Move','Immediate Possession'))
+
+x_test = pd.DataFrame(columns=['area_type','size','total_sqft', 'bath', 'balcony', 'extract'],
+             data=np.array([area_type, size, total_sqft, bath, balcony, extract]).reshape(1,6))
+
+loaded_model_RF = joblib.load(r'app/2nd_model_RF.joblib')
+y_pred = loaded_model_RF.predict(x_test)
+
+
+price = np.expm1(y_pred)
+if st.button('Show Price'):
+    st.write("Price of House:", price)
